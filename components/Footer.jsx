4be986bc6df05dc4df51d6c7efb8cd667da8bf36
Bou-Mery <141419669+Bou-Mery@users.noreply.github.com>
2024-08-22 12:06:17 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='footer flex-center'>
       <div className='footer-content'>
-        <p>&copy; 2024 INDATA-<span className='ocp'>OCP</span>. All rights reserved.</p>
+        <p>&copy; 2024 INDATA. All rights reserved.</p>
         <div className='social-links'>
           <a href='#' className='social-link'>LinkedIn</a>
           <a href='#' className='social-link'>Twitter</a>
